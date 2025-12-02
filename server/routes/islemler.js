97969dd886f5eafdb42f211570488dbd20934c8f
halildundar <halildundar.eee@gmail.com>
2025-12-02 13:31:02 +0300

    flush2

--- server/routes/islemler.js
+++ server/routes/islemler.js
@@ -35,7 +35,7 @@ export const GetIslemlerWatch = (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache,no-transform");
   res.setHeader("Connection", "keep-alive");
-  res.setHeader("Content-Encoding", "none");
+  res.setHeader("Content-Encoding", "identity");
   res.setHeader('X-Accel-Buffering', 'no');
   // res.setHeader('Access-Control-Allow-Origin','*');
   res.flushHeaders(); 
